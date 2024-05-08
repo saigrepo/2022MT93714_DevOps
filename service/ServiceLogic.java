821ba09e4f4f79548ae0d9c12693d426d91febfb
Saiganesh2023 <iasganesh123456@gmail.com>
2024-05-08 20:42:12 +0530

    first commit

---
+++ service/ServiceLogic.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import model.Model;
+
+class ServiceLogic {
+
+    public static void main() {
+        Model model = new Model("Devops Assignment");
+        List<Model> lst= new ArrayList<>();
+
+    }
+}
