38743876801e47df759b40c1455853223251e6d4
Saiganesh2023 <iasganesh123456@gmail.com>
2024-05-08 20:51:38 +0530

    commit changes

--- model/Model.java
+++ model/Model.java
@@ -1,17 +1,13 @@
 package model;
 
 class Model {
-
     String modelName;
-
     public Model(String modelName) {
         this.modelName = modelName;
     }
-
     public String getModelName() {
         return modelName;
     }
-
     public void setModelName(String modelName) {
         this.modelName = modelName;
     }
--- service/ServiceLogic.java
+++ service/ServiceLogic.java
@@ -6,6 +6,5 @@ class ServiceLogic {
     public static void main() {
         Model model = new Model("Devops Assignment");
         List<Model> lst= new ArrayList<>();
-
     }
 }
